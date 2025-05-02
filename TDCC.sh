fb5ea21ea276690dff5de22ca2a432db50f53ceb
Yuchen Zhu <90080453+H1nkik@users.noreply.github.com>
2025-05-02 15:34:05 +0800

    Update and rename TDEC.sh to TDCC.sh

--- TDCC.sh
+++ TDCC.sh
@@ -2,8 +2,8 @@
 #JSUB -q gpu
 #JSUB -n 4
 #JSUB -gpgpu 1
-#JSUB -e errorTDEC-STL.txt
-#JSUB -o outputTDEC-STL.txt
-#JSUB -J TDECGPU
+#JSUB -e errorTDCC-STL.txt
+#JSUB -o outputTDCC-STL.txt
+#JSUB -J TDCCGPU
 ./data/users/zhouk/software/anaconda3/bin/python3
-./TDEC.py
+./TDCC.py
