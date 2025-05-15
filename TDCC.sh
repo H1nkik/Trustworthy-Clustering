7d64d3ae79758673830fb3b6ed3e9a6525977a25
Yuchen Zhu <90080453+H1nkik@users.noreply.github.com>
2025-05-15 17:43:56 +0800

    Delete TDCC.sh

--- TDCC.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#JSUB -q gpu
-#JSUB -n 4
-#JSUB -gpgpu 1
-#JSUB -e errorTDCC-STL.txt
-#JSUB -o outputTDCC-STL.txt
-#JSUB -J TDCCGPU
-./data/users/zhouk/software/anaconda3/bin/python3
-./TDCC.py
